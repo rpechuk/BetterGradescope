3954a64e94c3a28b345832e55900183f19d667fb
ronpechuk <rpechuk@gmail.com>
2023-10-14 15:21:40 -0700

    theme change time

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
+    var r = document.querySelector(':root') as HTMLElement;
+    r.style.setProperty('--tdl-color-primary', '#00FF00');
     console.log("content view loaded");
   }, []);
 
