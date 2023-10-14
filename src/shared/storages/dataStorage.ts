de63130a86e511a0d8811d7d690269f07db72f43
ronpechuk <rpechuk@gmail.com>
2023-10-14 16:49:03 -0700

    added storage

---
+++ src/shared/storages/dataStorage.ts
@@ -0,0 +1,39 @@
+import {
+  BaseStorage,
+  createStorage,
+  StorageType,
+} from "@src/shared/storages/base";
+
+export type User = {
+  quarter?: {
+    class: {
+      name: string,
+      href: string,
+      assignments: {
+        name: string,
+        href: string,
+        dueDate: string,
+        submitted: boolean,
+        points: number,
+        totalPoints: number,
+      }[]
+    }[]
+  }[]
+};
+
+type DataStorage = BaseStorage<User> & {
+  setUserInfo: (user: User) => void;
+};
+
+const storage = createStorage<User>("gradescope-user-info", {}, {
+  storageType: StorageType.Local,
+});
+
+const dataStorage: DataStorage = {
+  setUserInfo: (user) => {
+    dataStorage.set(user);
+  },
+  ...storage,
+};
+
+export default dataStorage;
