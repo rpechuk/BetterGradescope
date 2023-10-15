31ff3a313e48fe0a16d92a36672aa59026fbdfd3
jasmineyearian <jasmineyearian@gmail.com>
2023-10-14 22:47:15 -0700

    Added line

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    
+    console.log("Component has updated!"); 
   }, []);
 
   return <div className="text-lime-400">content view</div>;
