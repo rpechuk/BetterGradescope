f42b5c0080885db379ef19175f296383f8163909
ronpechuk <rpechuk@gmail.com>
2023-10-14 21:30:07 -0700

    fixed popup

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -10,28 +10,6 @@ const Popup = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-lime-400">
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <button
-          style={{
-            color: theme === "light" ? "#fff" : "#000",
-          }}
-          onClick={exampleThemeStorage.toggle}
-        >
-          Toggle theme: [{theme}]
-        </button>
-      </header>
     </div>
   );
 };
