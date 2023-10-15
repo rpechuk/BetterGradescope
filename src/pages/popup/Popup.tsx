9b3bf663427e2bb668126eb433a685c306234a1c
ronpechuk <rpechuk@gmail.com>
2023-10-14 22:30:52 -0700

    added tabs

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,16 +1,53 @@
 import React from "react";
-import logo from "@assets/img/logo.svg";
 import "@pages/popup/Popup.css";
 import useStorage from "@src/shared/hooks/useStorage";
 import exampleThemeStorage from "@src/shared/storages/exampleThemeStorage";
 import withSuspense from "@src/shared/hoc/withSuspense";
+import { SettingFilled, CheckCircleFilled } from '@ant-design/icons';
+import { ConfigProvider, Tabs } from 'antd';
+import { ColorPicker } from 'antd';
+
+const { TabPane } = Tabs;
 
 const Popup = () => {
-  const theme = useStorage(exampleThemeStorage);
 
   return (
-    <div className="App">
-    </div>
+      <ConfigProvider
+      theme={{
+        components: {
+          Tabs: {
+            itemColor: '#white',
+            itemSelectedColor: '#6e6e6e',
+            itemActiveColor: 'white',
+            itemHoverColor: 'white',
+          },
+        },
+      }}
+    >
+      <div className="App py-3 justify-center">
+        <Tabs 
+          size="large"
+          defaultActiveKey="1"
+          className="w-full"
+          centered={true}
+          type="card"
+          style={{ color: 'white'}}
+        >
+          <TabPane
+            tab="Todo"
+            key="1"
+          >
+            Content of Tab 1
+          </TabPane>
+          <TabPane
+            tab="Settings"
+            key="2"
+          >
+            Content of Tab 2
+          </TabPane>
+        </Tabs>
+      </div>
+    </ConfigProvider>
   );
 };
 
