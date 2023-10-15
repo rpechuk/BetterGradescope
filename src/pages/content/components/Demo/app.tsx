c561c85b1526a5451e5a44c945ababe437e29128
ronpechuk <rpechuk@gmail.com>
2023-10-14 23:10:02 -0700

    merged finished

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -6,10 +6,7 @@ chrome.storage.onChanged.addListener((changed) =>{
 
 export default function App() {
   useEffect(() => {
-<<<<<<< HEAD
-=======
     console.log("Component has updated!"); 
->>>>>>> 31ff3a313e48fe0a16d92a36672aa59026fbdfd3
   }, []);
 
   return <div className="text-lime-400">content view</div>;
